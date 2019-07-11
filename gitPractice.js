771e484a713e0b1ca3fb9723858f1b7aa40d3817
qamarcASC6 <qamarchisti292@gmail.com>
2019-07-11 11:46:42 -0700

    create for loop to multiply array values

---
+++ gitPractice.js
@@ -0,0 +1,24 @@
+// function that multiplies all values in array by 2 and prints the new array
+//someArray = [10, 20, 30 , 40]
+// 10 20 30 40 
+function multiplyArrayNumbers(arr) {
+    //create a loop
+
+    //that will go through each individual element if the array
+
+    //and multiply the value by 2
+
+    for(i = 0; i < arr.length; i++) {
+    // that will go through each indiviual element of the array
+    //and multiply the value by 2 
+    arr[i] = (arr[i])*2 //arr[i]*=2
+    }
+    
+
+
+    arr = [ 5, 10 , 15, 20 ]
+    for(let i = 0; i < arr.length; i++);{
+        console.log (arr[1]);
+    }   
+
+}
